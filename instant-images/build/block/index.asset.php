6529a759de29b0406acdb8375e701a1e4605e254
Justin Gardner <justin@rhynorater.com>
2025-01-05 06:01:44 -0500

    7.0.1

--- instant-images/build/block/index.asset.php
+++ instant-images/build/block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '81c099ea3fca1106f2d2');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c010bb874840a553bc84');
